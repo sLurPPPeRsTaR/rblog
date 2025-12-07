856ae4d959fac7887695b6a14110a8e7d637dfa2
raymond <antisocialraymond@gmail.com>
2025-12-07 13:46:30 +0700

    chore: add username when register, add login via username or email, add username when edit profile (Modifikasi Login & Registrasi)

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,14 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => [
+            'username' => [
+                'required',
+                'string',
+                'min:4',
+                'max:16',
+                'alpha_num',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],   'email' => [
                 'required',
                 'string',
                 'lowercase',
